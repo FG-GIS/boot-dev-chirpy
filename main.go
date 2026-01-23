dcb7af19315f5cd1a6ba2d0e3a7f4ddbff757b3b
Giovanni Flore <flore.giovanni.scripting@outlook.com>
2026-01-23 12:55:58 +0100

    CH4 L2 JSON

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -37,6 +38,48 @@ func (cfg *apiConfig) metricsReset(w http.ResponseWriter, r *http.Request) {
 	w.Write(fmt.Appendf(res, "Hits reset from: %v\nTo: 0", cfg.fileserverHits.Swap(0)))
 }
 
+func respondWithError(w http.ResponseWriter, code int, errorMsg string) {
+	log.Print(errorMsg)
+	w.WriteHeader(code)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Error marshaling JSON: %s", err))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func validationHandler(w http.ResponseWriter, r *http.Request) {
+	type chirp struct {
+		Body string `json:"body"`
+	}
+	type validated struct {
+		ValidCheck bool `json:"valid"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	message := chirp{}
+	err := decoder.Decode(&message)
+	code := 200
+	if err != nil {
+		respondWithError(w, 500, fmt.Sprintf("Error decoding the message: %s", err))
+	}
+	respBody := validated{
+		ValidCheck: true,
+	}
+	if len([]rune(message.Body)) > 140 {
+		respBody.ValidCheck = false
+		code = 400
+		respondWithError(w, code, "Chirp is too long.")
+	}
+	respondWithJSON(w, code, respBody)
+}
+
 func main() {
 	apiCfg := apiConfig{}
 	port := "8080"
@@ -53,6 +96,7 @@ func main() {
 	})
 	mux.HandleFunc("GET "+adminPath+"/metrics", apiCfg.metricsEnd)
 	mux.HandleFunc("POST "+adminPath+"/reset", apiCfg.metricsReset)
+	mux.HandleFunc("POST "+apiPath+"/validate_chirp", validationHandler)
 
 	server := &http.Server{
 		Handler: mux,
